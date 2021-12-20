14ff7fefc409aaa201de382238f909672b504ffd
Maciej Krüger <mkg20001@gmail.com>
2021-12-20 05:42:46 +0100

    static-ip: ipv6 support

--- package/gluon-static-ip/check_site.lua
+++ package/gluon-static-ip/check_site.lua
@@ -1,2 +1,6 @@
-need_string({'tmpIp4'})
-need_number({'tmpIp4Range'})
+-- TODO: conditional range required when tmp set
+
+need_string({'tmpIp4'}, false)
+need_number({'tmpIp4Range'}, false)
+need_string({'tmpIp6'}, false)
+need_number({'tmpIp6Range'}, false)
