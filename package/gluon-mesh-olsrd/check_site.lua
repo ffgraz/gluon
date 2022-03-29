f5d5288fbb1d527f3c404ef6f1388ce5d715d937
Maciej Krüger <mkg20001@gmail.com>
2022-03-29 14:38:09 +0200

    olsr check site

--- package/gluon-mesh-olsrd/check_site.lua
+++ package/gluon-mesh-olsrd/check_site.lua
@@ -1,2 +1,13 @@
-need_boolean({'mesh', 'olsrd', 'v4', 'enable'}, false)
-need_boolean({'mesh', 'olsrd', 'v6', 'enable'}, false)
+if need_boolean({'mesh', 'olsrd', 'v1', 'enable'}, false) then
+  need_table({'mesh', 'olsrd', 'v1', 'config'}, nil, false)
+end
+
+if need_boolean({'mesh', 'olsrd', 'v2', 'enable'}, false) then
+  need_table({'mesh', 'olsrd', 'v2', 'config'}, nil, false)
+  need_boolean({'mesh', 'olsrd', 'v2', 'ip6_exclusive_mode'}, false)
+  need_boolean({'mesh', 'olsrd', 'v2', 'ip4_exclusive_mode'}, false)
+  if need_boolean({'mesh', 'olsrd', 'v2', 'ip4_exclusive_mode'}, false) and need_boolean({'mesh', 'olsrd', 'v2', 'ip6_exclusive_mode'}, false) then
+    -- FIXME: we could check the value but idk how to do that. basically both options are xor.
+    error('you cant enable both olsrv2 ip4 and ip6 exclusive mode')
+  end
+end
