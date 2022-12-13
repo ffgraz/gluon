e4a1cd5cdaac2f4d85e876870d33bf135afdd604
Maciej Krüger <mkg20001@gmail.com>
2022-12-13 22:52:00 +0100

    [patch] losen some requirements so we can fuck things up more efficiently

--- package/gluon-core/check_site.lua
+++ package/gluon-core/check_site.lua
@@ -27,7 +27,7 @@ need_string(in_site({'timezone'}))
 need_string_array({'ntp_servers'}, false)
 
 need_string_match(in_domain({'prefix4'}), '^%d+.%d+.%d+.%d+/%d+$', false)
-need_string_match(in_domain({'prefix6'}), '^[%x:]+/64$')
+need_string_match(in_domain({'prefix6'}), '^[%x:]+/%d+$')
 need_string_array_match(in_domain({'extra_prefixes6'}), '^[%x:]+/%d+$', false)
 
 local supported_rates = {6000, 9000, 12000, 18000, 24000, 36000, 48000, 54000}
@@ -72,7 +72,7 @@ end
 need_boolean(in_site({'poe_passthrough'}), false)
 
 if need_table({'dns'}, nil, false) then
-	need_string_array_match({'dns', 'servers'}, '^[%x:]+$')
+	-- need_string_array_match({'dns', 'servers'}, '^[%x:]+$')
 end
 
 need_string_array(in_domain({'next_node', 'name'}), false)
