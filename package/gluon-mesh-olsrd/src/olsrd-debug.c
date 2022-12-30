532e5b48dcad8abb86052af30f2a6c14cf67be5b
Maciej Krüger <mkg20001@gmail.com>
2022-12-30 16:48:08 +0100

    olsr: respondd works, bitches

--- package/gluon-mesh-olsrd/src/olsrd-debug.c
+++ package/gluon-mesh-olsrd/src/olsrd-debug.c
@@ -1,6 +1,6 @@
 #include <libolsrdhelper.h>
-#include "respondd-neighbours.c"
-#include "respondd-nodeinfo.c"
+#include "respondd-common.h"
+#include <stdio.h>
 
 void d(const char * fnc, json_object * res) {
   if (!res) {
@@ -21,13 +21,15 @@ void d2(const char * fnc, json_object * res, int code) {
 int main (int argc, char *argv[]) {
   json_object *resp;
 
-  d("provider_neighbours", respondd_provider_neighbours());
+  d("provider_neighbours", real_respondd_provider_neighbours());
 
-  d("provider_nodeinfo", respondd_provider_nodeinfo());
+  d("provider_nodeinfo", real_respondd_provider_nodeinfo());
 
   d2("olsr1_nodeinfo", resp, olsr1_get_nodeinfo("all", &resp));
 
   d2("olsr2_nodeinfo", resp, olsr2_get_nodeinfo("nhdpinfo jsonraw link", &resp));
+
+  d("make_safe", make_safe("n"));
 
   return 0;
 }
