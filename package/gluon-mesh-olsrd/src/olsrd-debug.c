d1f3e687df263c4f8ee92bb33908bb893f84796a
Maciej Krüger <mkg20001@gmail.com>
2022-12-30 15:41:38 +0100

    olsr: fixup c code

--- package/gluon-mesh-olsrd/src/olsrd-debug.c
+++ package/gluon-mesh-olsrd/src/olsrd-debug.c
@@ -1,20 +1,33 @@
 #include <libolsrdhelper.h>
-#include "respondd-common.h"
+#include "respondd-neighbours.c"
+#include "respondd-nodeinfo.c"
+
+void d(const char * fnc, json_object * res) {
+  if (!res) {
+    printf("%s: failed\n\n", fnc);
+  } else {
+    printf("%s: %s\n\n", fnc, json_object_to_json_string_ext(res, JSON_C_TO_STRING_PLAIN));
+  }
+}
+
+void d2(const char * fnc, json_object * res, int code) {
+  if (code) {
+    printf("%s: failed %i\n\n", fnc, code);
+  } else {
+    printf("%s: %s\n\n", fnc, json_object_to_json_string_ext(res, JSON_C_TO_STRING_PLAIN));
+  }
+}
 
 int main (int argc, char *argv[]) {
-  struct json_object *resp;
+  json_object *resp;
 
-  if (!respondd_provider_neighbours())
-    return 1;
+  d("provider_neighbours", respondd_provider_neighbours());
 
-  if (!respondd_provider_nodeinfo())
-    return 1;
+  d("provider_nodeinfo", respondd_provider_nodeinfo());
 
-  if (olsr1_get_nodeinfo("all", &resp))
-    return 1;
+  d2("olsr1_nodeinfo", resp, olsr1_get_nodeinfo("all", &resp));
 
-  if (olsr2_get_nodeinfo("nhdpinfo jsonraw link", &resp))
-    return 1;
+  d2("olsr2_nodeinfo", resp, olsr2_get_nodeinfo("nhdpinfo jsonraw link", &resp));
 
   return 0;
 }
