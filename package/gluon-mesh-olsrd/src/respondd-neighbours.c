bd8601380a2fa938bd8097dd2a0b8aaabe59fb6e
Maciej Krüger <mkg20001@gmail.com>
2022-12-30 15:41:38 +0100

    olsr: fixup c code

--- package/gluon-mesh-olsrd/src/respondd-neighbours.c
+++ package/gluon-mesh-olsrd/src/respondd-neighbours.c
@@ -39,27 +39,43 @@ struct json_object * real_respondd_provider_neighbours(void) {
 	struct olsr_info *info;
 
 	struct json_object *ret = json_object_new_object();
+	struct json_object *out = json_object_new_object();
 
 	if (oi(&info))
 		return ret;
 
-	struct json_object *neigh = json_object_new_array();
+	struct json_object *neighs = json_object_new_object();
 
 	if (info->olsr1.enabled && info->olsr1.running) {
 		struct json_object *neigh1 = olsr1_get_neigh();
 		if (neigh1)
-			for (int i = 0; i < json_object_array_length(neigh1); i++)
-				json_object_array_add(neigh, json_object_array_get_idx(neigh1, i));
+			merge_neighs(neighs, neigh1, "olsr1");
 	}
 
 	if (info->olsr2.enabled && info->olsr2.running) {
 		struct json_object *neigh2 = olsr2_get_neigh();
 		if (neigh2)
-			for (int i = 0; i < json_object_array_length(neigh2); i++)
-				json_object_array_add(neigh, json_object_array_get_idx(neigh2, i));
+			merge_neighs(neighs, neigh2, "olsr2");
 	}
 
-	json_object_object_add(ret, "neigh", neigh);
+	json_object_object_foreach(neighs, mac, neigh) {
+		const char * ifname = json_object_get_string(json_object_object_get(neigh, "ifname"));
+
+		if (!ifname) {
+			continue;
+		}
+
+		json_object * intf = json_object_object_get(out, ifname);
+
+		if (!intf) {
+			intf = json_object_new_object();
+			json_object_object_add(out, ifname, intf);
+		}
+
+		json_object_object_add(intf, mac, neigh);
+	}
+
+	json_object_object_add(ret, "batadv", out);
 
 	return ret;
 }
