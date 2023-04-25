808c2a05cf9d71902741fafc14551e91813f08f7
Maciej Krüger <mkg20001@gmail.com>
2023-04-25 17:50:10 +0200

    [patch] adjust push_pkg for graz opkg-less devices

--- contrib/push_pkg.sh
+++ contrib/push_pkg.sh
@@ -7,7 +7,6 @@ topdir="$(realpath "$(dirname "${0}")/../openwrt")"
 # defaults to qemu run script
 ssh_host=localhost
 build_only=0
-preserve_config=1
 
 print_help() {
 	echo "$0 [OPTIONS] PACAKGE_DIR [PACKAGE_DIR] ..."
@@ -18,10 +17,6 @@ print_help() {
 	echo "             running qemu instance started by run_qemu.sh."
 	echo " -p PORT     use PORT as ssh port (default is 22)"
 	echo " -b          build only, do not push"
-	echo " -P          do not preserve /etc/config. By default, if a package"
-	echo "             defines a config file in /etc/config, this config file"
-	echo "             will be preserved. If you specify this flag, the package"
-	echo "             default will be installed instead."
 	echo ""
 	echo ' To change gluon variables, run e.g. "make config GLUON_MINIFY=0"'
 	echo ' because then the gluon logic will be triggered, and openwrt/.config'
@@ -33,7 +28,6 @@ print_help() {
 while getopts "p:r:hbP" opt
 do
 	case $opt in
-		P) preserve_config=0;;
 		p) ssh_port="${OPTARG}";;
 		r) ssh_host="${OPTARG}"; [ -z "$ssh_port" ] && ssh_port=22;;
 		b) build_only=1;;
@@ -121,18 +115,16 @@ while [ $# -gt 0 ]; do
 			fi
 		done
 
-		if [ "$preserve_config" -eq 0 ]; then
-			opkg_flags=" --force-maintainer"
-		fi
-
 		# shellcheck disable=SC2029
 		if [ -n "$filename" ]; then
-			scp -O -P "${ssh_port}" "$feed/$filename" "root@${BL}${ssh_host}${BR}:/tmp/${filename}"
+			scp -P "${ssh_port}" "$feed/$filename" "root@${BL}${ssh_host}${BR}:/tmp/${filename}"
 			ssh -p "${ssh_port}" "root@${ssh_host}" "
 				set -e
-				echo Running opkg:
-				opkg install --force-reinstall ${opkg_flags} '/tmp/${filename}'
+				echo Extracting:
+				tar xvfz '/tmp/${filename}' -C /tmp
+				tar xvfz '/tmp/data.tar.gz' -C /
 				rm '/tmp/${filename}'
+				rm /tmp/*.tar.gz
 				gluon-reconfigure
 			"
 		else
